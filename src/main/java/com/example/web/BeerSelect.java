6a69c0b46da69c766944880ce7733c81370793f9
Victor Arana <victor_arana@hotmail.com>
2014-07-06 19:42:35 -0500

    Primera versión del proyecto Beer

---
+++ src/main/java/com/example/web/BeerSelect.java
@@ -0,0 +1,23 @@
+package com.example.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BeerSelect extends HttpServlet {
+       
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Beer Selection Advice <br>");
+		String c = request.getParameter("color");
+		out.println("<br>Got beer color " + c);
+	}
+
+}

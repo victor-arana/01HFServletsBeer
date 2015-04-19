851f8ecea16a8fc3eff60ff2a05a800476de2b73
Victor Jose Arana Rodriguez <victor_arana@hotmail.com>
2015-04-19 01:15:51 -0500

    First Commit

--- src/main/java/com/example/web/BeerSelect.java
+++ src/main/java/com/example/web/BeerSelect.java
@@ -2,22 +2,34 @@ package com.example.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.model.BeerExpert;
+
 public class BeerSelect extends HttpServlet {
        
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String c = request.getParameter("color");
+		BeerExpert be = new BeerExpert();
+		List<String> result = be.getBrands(c);
+		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("Beer Selection Advice <br>");
-		String c = request.getParameter("color");
-		out.println("<br>Got beer color " + c);
+		
+		Iterator<String> it = result.iterator();
+		while(it.hasNext()){
+			out.print("<br>try: " + it.next());
+		}
 	}
 
 }
